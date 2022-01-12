341d8ac35d5a775b0074fe2893f5580e53a7131a
its0beno <boodebeno@gmail.com>
2022-01-12 13:36:32 +0300

    modified forms

--- hms/hospital/forms.py
+++ hms/hospital/forms.py
@@ -1,5 +1,8 @@
 from django import forms
+from django.db import models
 
+
+CHOICES = [("Female","Female"), ("Male","Male") ]
 
 class RegisterPatientForm(forms.ModelForm):
     first_name = forms.CharField(max_length=20, min_length=2, required=True, widget=forms.TextInput(attrs={
@@ -18,20 +21,15 @@ class RegisterPatientForm(forms.ModelForm):
         'type':'number',
         'placeholder':'Phone number'
     }))
-    b_date = forms.DateField(required= True, widget=forms.TextInput(attrs={
-        'type':'date',
-        'placeholder':'Birth Date'
-    } ))
-    email = forms.EmailField(required= True,  widget=forms.TextInput(attrs={
-        'type':'Email',
+    b_date = forms.DateField(required= True)
+    email = forms.EmailField(required= False,  widget=forms.TextInput(attrs={
+        'type':'email',
         'placeholder':'Email'
     }))
-    sex = forms.CharField(required= True,  widget=forms.TextInput(attrs={
-        'type':'Gender',
-        'placeholder':'Sex'
-    }))
+    sex = forms.ChoiceField( choices=[CHOICES], required=True)
     blood_type = forms.CharField(required= True ,widget=forms.TextInput(attrs={
         'type':'Blood Type',
         'placeholder':'Blood Type'
     }))
-    
+class RegisterPatientCondition(forms.ModelForm):
+     symtopms = forms.
