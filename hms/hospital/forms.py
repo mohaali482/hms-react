b374ccb9e6468e06c7519a5c70c0ef9e671bec20
Mohammed Ali <mohaali482@gmail.com>
2022-01-15 10:48:14 +0300

    added labels for the forms

--- hms/hospital/forms.py
+++ hms/hospital/forms.py
@@ -10,29 +10,29 @@ GenderChoices = [("Female","Female"), ("Male","Male") ]
 BloodTypes = [("A+","A+" ), ("A-", "A-"), ("B-","B-"),("B+", "B+"),("AB+","AB+"), ("AB-","AB-"),("O-","O-"),("O+", "O+")]
 
 class RegisterPatientForm(forms.ModelForm):
-    first_name = forms.CharField(max_length=20, min_length=2, required=True, widget=forms.TextInput(attrs={
+    first_name = forms.CharField(label = "First Name",max_length=20, min_length=2, required=True, widget=forms.TextInput(attrs={
         'type':'name',
         'placeholder':'First Name'
     }))
-    middle_name = forms.CharField(max_length=20, min_length=2, required=True, widget=forms.TextInput(attrs={
+    middle_name = forms.CharField(label = "Middle Name",max_length=20, min_length=2, required=True, widget=forms.TextInput(attrs={
         'type':'name',
         'placeholder':'Middle Name'
     }))
-    last_name = forms.CharField(max_length=20, min_length=2, required=True, widget=forms.TextInput(attrs={
+    last_name = forms.CharField(label = "Last Name",max_length=20, min_length=2, required=True, widget=forms.TextInput(attrs={
         'type':'name',
         'placeholder':'Last Name'
     }))
-    phone = forms.CharField( max_length=13, required=True, min_length=2 , widget=forms.TextInput(attrs={
+    phone = forms.CharField(label = "Phone Number", max_length=13, required=True, min_length=2 , widget=forms.TextInput(attrs={
         'type':'number',
         'placeholder':'Phone number'
     }))
-    b_date = forms.DateField(required= True)
-    email = forms.EmailField(required= False,  widget=forms.TextInput(attrs={
+    b_date = forms.DateField(label = "Birth Date",required= True)
+    email = forms.EmailField(label = "Email",required= False,  widget=forms.TextInput(attrs={
         'type':'email',
         'placeholder':'Email'
     }))
-    sex = forms.ChoiceField( choices=[GenderChoices ], required=True)
-    blood_type = forms.ChoiceField( choices=[BloodTypes], required=False)
+    sex = forms.ChoiceField(label = "Sex", choices=[GenderChoices ], required=True)
+    blood_type = forms.ChoiceField( label = "Blood Type",choices=[BloodTypes], required=False)
 
     class Meta :
         model = Patient
