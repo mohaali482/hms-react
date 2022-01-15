a3a5d636be44f52b214b2f0ff891c5e2546e7796
Mohammed Ali <mohaali482@gmail.com>
2022-01-15 09:01:37 +0300

    changed hopital url to hospital/

--- hms/hms/urls.py
+++ hms/hms/urls.py
@@ -21,5 +21,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hospital', include('hospital.urls') )
+    path('hospital/', include('hospital.urls') )
 ]
