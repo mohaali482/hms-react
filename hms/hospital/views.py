c47a3f2dd257a686f0c258112d873fac4568683a
Mohammed Ali <mohaali482@gmail.com>
2022-01-15 10:35:14 +0300

    made forms in index function

--- hms/hospital/views.py
+++ hms/hospital/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import context
+from .forms import *
 # Create your views here.
 
 
 def home(request):
     return render(request, 'hospital/dashboard.html')
 def index(request):
-    return render(request, 'hospital/base.html')
+    form = RegisterPatientForm(request.POST or None)
+    # if form.is_valid():
+    #     return HttpResponse('done')
+
+    context = {}
+    context['form'] = form
+    context['text'] = 'Add a Patient'
+    context['general'] = 'Patient Information'
+    return render(request, 'hospital/base.html', context)
