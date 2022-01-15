05867a6d10fa8c1b4522868c3054b1affa0e6c7e
Mohammed Ali <mohaali482@gmail.com>
2022-01-15 15:57:33 +0300

    added a function to logout the user

--- hms/hms/views.py
+++ hms/hms/views.py
@@ -1,17 +1,8 @@
-from django.contrib.auth import authenticate, login, views
-from django.shortcuts import render
+from django.contrib.auth import logout as logout_
+from django.shortcuts import redirect, render
 # Create your views here.
 
 
-def login_page(request):
-    if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-
-        user = authenticate(request, username, password)
-        if user is not None:
-            login()
-        else:
-            pass
-    else:
-        return render(request, 'hospital/login.html',{})
+def logout(request):
+    logout_(request)
+    return redirect('login')
