750d8174884751ccc1dd716c608cd04d0bc5be98
its0beno <boodebeno@gmail.com>
2022-02-06 16:09:12 +0300

    edited the comments on the url and views

--- hms/hms/views.py
+++ hms/hms/views.py
@@ -2,12 +2,12 @@ from django.contrib.auth import logout as logout_
 from django.shortcuts import redirect, render
 # Create your views here.
 
-# we are defining our home page 
+#defining our home page 
 def home(request):
     return redirect('home-hospital')
 
 
-#we are redirectng our user to login page after logout
+#redirectng the user to login page after logout
 def logout(request):
     logout_(request)
     return redirect('login')
