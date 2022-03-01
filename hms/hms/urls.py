4c7d92bfcc28fdf0ae07e2d4fc90f09ce5f30178
Mohammed Ali <mohaali482@gmail.com>
2022-03-01 14:17:37 +0300

    start

--- hms/hms/urls.py
+++ hms/hms/urls.py
@@ -15,17 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import path,include
+from django.urls import path, include
 
 from . import views
 from django.urls import path
 
-#linking urls to views
+# linking urls to views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('', include('hospital.urls')),
-    path('login/', auth_views.LoginView.as_view(template_name='hospital/login.html'), name = 'login'),
-    path('logout/',views.logout,name='logout')
+    path('api', include('api.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='hospital/login.html'), name='login'),
+    path('logout/', views.logout, name='logout')
 ]
